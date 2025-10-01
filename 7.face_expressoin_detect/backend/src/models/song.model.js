32bea291a2e96c680965609d874e2b7c37c13edc
nikhilborge <73777445+nikhilborge@users.noreply.github.com>
2025-10-01 17:54:22 +0530

    day 10 get song api with query parmas

--- 7.face_expressoin_detect/backend/src/models/song.model.js
+++ 7.face_expressoin_detect/backend/src/models/song.model.js
@@ -4,6 +4,7 @@ const songsSchema = new mongoose.Schema({
   title: String,
   artist: String,
   audio: String,
+  mood: String,
 });
 
 const song = mongoose.model("song", songsSchema);
--- 7.face_expressoin_detect/backend/src/routes/song.routes.js
+++ 7.face_expressoin_detect/backend/src/routes/song.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const uplodFile = require("../service/storage.service");
+const songModel = require("../models/song.model");
 
 const router = express.Router();
 
@@ -13,11 +14,24 @@ router.post("/song", upload.single("audio"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   const fileData = await uplodFile(req.file);
+  const song = await songModel.create({
+    title: req.body.title,
+    artist: req.body.artist,
+    audio: fileData,
+    mood: req.body.mood,
+  });
   console.log(fileData);
   res.status(201).json({
     message: "song created successfully",
-    song: req.body,
+    song,
   });
 });
 
+router.get("/songs", async (req, res) => {
+  const { mood } = req.query;
+  const songs = await songModel.find({
+    mood,
+  });
+  res.status(200).json({ message: "songs fetch successfully", songs });
+});
 module.exports = router;
