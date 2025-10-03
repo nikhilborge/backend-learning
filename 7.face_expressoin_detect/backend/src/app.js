8beca24a563a6e069fe5e5b69095f9a57b4efab1
nikhilborge <73777445+nikhilborge@users.noreply.github.com>
2025-10-03 17:05:32 +0530

    day 10 - cors setup and other changes

--- 7.face_expressoin_detect/backend/src/app.js
+++ 7.face_expressoin_detect/backend/src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const songRoutes = require("./routes/song.routes");
-const app = express();
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/", songRoutes);
-
-
 
 module.exports = app;
