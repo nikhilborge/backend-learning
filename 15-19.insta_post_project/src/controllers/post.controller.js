9934ff0ae604cc29832added7203ed52a601c073
nikhilborge <73777445+nikhilborge@users.noreply.github.com>
2025-10-17 14:16:22 +0530

    day 19 -  image kit setup
    ai system instruction added, post controller update,  image kit file upload service setup

--- 15-19.insta_post_project/src/controllers/post.controller.js
+++ 15-19.insta_post_project/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const postModel = require("../models/post.model");
 const generateCaption = require("../service/ai.service");
+const uploadFile = require("../service/storage.service");
+const { v4: uuidv4 } = require("uuid");
 
 async function createPostController(req, res) {
   const file = req.file;
@@ -7,10 +9,17 @@ async function createPostController(req, res) {
 
   const base64Image = new Buffer.from(file.buffer).toString("base64");
   const caption = await generateCaption(base64Image);
-  console.log(caption, "generated caption");
+  const result = await uploadFile(file.buffer, `${uuidv4()}`);
 
-  res.json({
-    caption,
+  const post = await postModel.create({
+    caption: caption,
+    image: result.url,
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
   });
 }
 
